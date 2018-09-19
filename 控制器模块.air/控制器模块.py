981004ddbad49b4cb24d885856372d2edcb691b4
顾豪 <guhao_814659370@qq.com>
2018-09-19 15:10:02 +0800

    Initial commit

---
+++ 控制器模块.air/控制器模块.py
@@ -0,0 +1,312 @@
+# -*- encoding=utf8 -*-
+__author__ = "guhao"
+
+from airtest.core.api import *
+from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+poco = AndroidUiautomationPoco()
+
+name="测试"
+
+auto_setup(__file__)
+
+start_app("com.thinkhome.v3")
+sleep(5)
+
+#手动输入：为空
+poco(text="设置").click()
+poco(text="设备管理").click()
+# poco("com.thinkhome.v3:id/toolbar_btn_setting").click()
+# poco("com.thinkhome.v3:id/toolbar_btn_setting").click()
+# poco(text="手动输入").click()
+# poco("com.thinkhome.v3:id/manually_enter_id").set_text("")
+# poco("com.thinkhome.v3:id/btn_ok").click()
+# try:
+#     assert_exists(Template(r"tpl1536719197979.png", record_pos=(-0.003, 0.529), resolution=(1080, 1920)), "手动输入：为空")
+# except:
+#     print("Error")
+# else:
+#     print("Ok") 
+#     
+# #手动输入：ID格式不正确
+# poco("com.thinkhome.v3:id/manually_enter_id").set_text("1")
+# poco("com.thinkhome.v3:id/btn_ok").click()
+# try:
+#     assert_exists(Template(r"tpl1536719983748.png", record_pos=(-0.001, 0.527), resolution=(1080, 1920)), "#手动输入：ID格式不正确")
+# except:
+#     print("Error")
+# else:
+#     print("Ok")
+#     
+# #手动输入：ID不存在
+# poco("com.thinkhome.v3:id/manually_enter_id").set_text("12345678")
+# poco("com.thinkhome.v3:id/btn_ok").click()
+# message = poco("android:id/message").get_text()
+# try:
+#     assert_equal(message,"控制器不存在","手动输入：ID不存在")
+# except:
+#     print("Error")
+# else:
+#     print("Ok")
+# finally:
+#     poco("android:id/button3").click()
+#     
+# #手动输入：已被该账号添加       
+# poco("com.thinkhome.v3:id/manually_enter_id").set_text("74780900")
+# poco("com.thinkhome.v3:id/btn_ok").click()
+# poco("com.thinkhome.v3:id/btn_active").click()
+# poco("com.thinkhome.v3:id/btn_active").click()
+# message = poco("android:id/message").get_text()
+# try:
+#     assert_equal(message,"控制器已被该帐号添加","手动输入：已被该账号添加")
+# except:
+#     print("Error")
+# else:
+#     print("Ok")
+# finally:
+#     poco("android:id/button3").click()
+#     poco("com.thinkhome.v3:id/toolbar_btn_back").click()
+#     poco("com.thinkhome.v3:id/toolbar_btn_back").click()
+#     
+# #手动输入：已被其他账号添加
+# poco("com.thinkhome.v3:id/manually_enter_id").set_text("97557754")
+# poco("com.thinkhome.v3:id/btn_ok").click()
+# message = poco("android:id/message").get_text()
+# try:
+#     assert_equal(message,"控制器已被其他帐号添加","手动输入：已被其他账号添加")
+# except:
+#     print("Error")
+# else:
+#     print("Ok")
+# finally:
+#     poco("android:id/button3").click()
+#     poco("com.thinkhome.v3:id/toolbar_btn_back").click()    
+#     poco("com.thinkhome.v3:id/toolbar_btn_back").click()
+#     
+# #控制器删除：主机下还有从机挂载
+# poco(text="ThinkID:66762741").long_click()
+# poco(text="更多设置").click()
+# poco("com.thinkhome.v3:id/btn_delete").click()
+# poco("android:id/button1").click()
+# message = poco("android:id/message").get_text()
+# try:
+#     assert_equal(message,"请先删除该控制器下挂载的从机","控制器删除：主机下还有从机挂载")
+# except:
+#     print("Error")
+# else:
+#     print("Ok")
+# finally:
+#     poco("android:id/button3").click()
+#     poco("com.thinkhome.v3:id/toolbar_btn_back").click()
+# 
+# #控制器删除：删除成功
+# for i in range(1,10):
+#     if  poco(text="ThinkID:33049825").exists():
+#         poco(text="ThinkID:33049825").long_click()
+#         poco(text="更多设置").click()
+#         poco("com.thinkhome.v3:id/btn_delete").click()
+#         poco("android:id/button1").click()
+#         break
+#     else:
+#         poco("com.thinkhome.v3:id/expand_listview").swipe([-0.02, -0.40])
+#         sleep(2)
+# 
+# if(poco(text="ThinkID:33049825").exists()):
+#     message="删除失败"
+# else:
+#     message="删除成功"
+# try:
+#     assert_equal(message,"删除成功","控制器删除：删除成功")
+# except:
+#     print("Error")
+# else:
+#     print("Ok")    
+
+#修改名称：为空
+poco(text="ThinkID：77670215").long_click()
+poco(text="名称").click()
+poco("com.thinkhome.v3:id/tv_name").set_text("")
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+message = poco("android:id/message").get_text()
+try:
+    assert_equal(message,"名称不能为空","修改名称：为空")
+except:
+    print("Error")
+else:
+    print("Ok")
+finally:
+    poco("android:id/button3").click()
+
+# 修改名称：包含<字符
+poco("com.thinkhome.v3:id/tv_name").set_text("<")
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+message = poco("android:id/message").get_text()
+try:
+    assert_equal(message,"数据包含特殊字符","修改名称：包含<字符")
+except:
+    print("Error")
+else:
+    print("Ok")
+finally:
+    poco("android:id/button3").click()
+
+# 修改名称：包含>字符
+poco("com.thinkhome.v3:id/tv_name").set_text(">")
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+message = poco("android:id/message").get_text()
+try:
+    assert_equal(message,"数据包含特殊字符","修改名称：包含>字符")
+except:
+    print("Error")
+else:
+    print("Ok")
+finally:
+    poco("android:id/button3").click()
+
+# 修改名称：包含&字符
+poco("com.thinkhome.v3:id/tv_name").set_text("&")
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+message = poco("android:id/message").get_text()
+try:
+    assert_equal(message,"数据包含特殊字符","修改名称：包含&字符")
+except:
+    print("Error")
+else:
+    print("Ok")
+finally:
+    poco("android:id/button3").click()
+
+# 修改名称：包含\字符
+poco("com.thinkhome.v3:id/tv_name").set_text("\\")
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+message = poco("android:id/message").get_text()
+try:
+    assert_equal(message,"数据包含特殊字符","修改名称：包含\字符")
+except:
+    print("Error")
+else:
+    print("Ok")
+finally:
+    poco("android:id/button3").click()
+    
+# # 修改名称：包含＇字符
+# poco("com.thinkhome.v3:id/tv_name").set_text("＇")
+# poco("com.thinkhome.v3:id/toolbar_right_text").click()
+# message = poco("android:id/message").get_text()
+# try:
+#     assert_equal(message,"数据包含特殊字符","修改名称：包含＇字符")
+# except:
+#     print("Error")
+# else:
+#     print("Ok")
+# finally:
+#     poco("android:id/button3").click()
+
+# 修改名称：正确输入
+poco("com.thinkhome.v3:id/tv_name").set_text(name)
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+poco(text="ThinkID：77670215").long_click()
+poco(text="更多设置").click()
+message = poco("com.thinkhome.v3:id/tv_name").get_text()
+try:
+    assert_equal(message,"name","修改名称：正确输入")
+except:
+    print("Error")
+else:
+    print("Ok")
+
+# 位置：拍照
+poco(text="位置").click()
+poco("com.thinkhome.v3:id/iv").click()
+poco(text="拍照").click()
+poco("com.huawei.camera:id/shutter_button").click()#考虑用图像识别
+poco("com.huawei.camera:id/btn_done").click()#考虑用图像识别
+poco("com.android.gallery3d:id/head_select_right").click()
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+
+# 位置：从相册选择
+poco(text="位置").click()
+poco("com.thinkhome.v3:id/iv").click()
+poco(text="选择本地图片").click()
+poco(text="最近").click()
+poco("com.android.documentsui:id/icon_mime").click()
+poco("com.android.gallery3d:id/head_select_right").click()
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+
+# 位置：删除
+poco(text="位置").click()
+poco("com.thinkhome.v3:id/iv").click()
+poco(text="删除").click()
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+
+# 位置：添加描述
+poco(text="位置").click()
+poco("com.thinkhome.v3:id/feedback").set_text(name)
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+
+# 功能停启用：停用
+poco(text="功能停启用").click()
+poco(text="A门锁1").click()
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+
+# 功能停启用：启用
+poco(text="功能停启用").click()
+poco(text="A门锁1").click()
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+
+# 反馈数据选项设置
+poco("com.thinkhome.v3:id/expand_listview").child("com.thinkhome.v3:id/detail_layout")[4].child("android.widget.RelativeLayout").child("com.thinkhome.v3:id/expand_img").click()
+for i in range(1,10):
+    if  poco(text="ThinkID:94299091").exists():
+        poco(text="ThinkID:33049825").long_click()
+        poco(text="更多设置").click()
+        poco(text="指示灯方案").click()
+        break
+    else:
+        poco("com.thinkhome.v3:id/expand_listview").swipe([-0.02, -0.40])
+        sleep(2)
+
+
+poco(text="反馈数据选项").click()
+poco(text="二氧化碳").click()
+poco(text="TVOC").click()
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+
+# 反馈数据选项：恢复默认
+poco(text="反馈数据选项").click()
+poco("com.thinkhome.v3:id/btn_restore").click()
+poco(text="反馈数据选项").click()
+
+# 夜间显示：设置
+poco(text="反馈数据选项").click()
+poco(text="昏暗不显示").click()
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+
+# 替换：为空
+poco("com.thinkhome.v3:id/expand_listview").child("com.thinkhome.v3:id/detail_layout")[0].child("android.widget.RelativeLayout").child("com.thinkhome.v3:id/expand_img").click()
+poco(text="ThinkID:94170840").click()
+poco(text="更多设置").click()
+poco(text="替换").click()
+poco("com.thinkhome.v3:id/edit_text_view").set_text("")
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+message = poco("com.thinkhome.v3:id/tv_name").get_text()
+try:
+    assert_equal(message,"内容不能为空","替换：为空")
+except:
+    print("Error")
+else:
+    print("Ok")
+finally:
+    poco("android:id/button3").click()
+    
+# 替换：控制器不存在
+poco("com.thinkhome.v3:id/edit_text_view").set_text("111")
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+message = poco("com.thinkhome.v3:id/tv_name").get_text()
+try:
+    assert_equal(message,"控制器不存在","替换：控制器不存在")
+except:
+    print("Error")
+else:
+    print("Ok")
+finally:
+    poco("android:id/button3").click()
---
+++ 账号模块.air/账号模块.py
@@ -0,0 +1,197 @@
+# -*- encoding=utf8 -*-
+__author__ = "guhao"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+
+from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+poco = AndroidUiautomationPoco()
+
+start_app("com.thinkhome.v3")
+sleep(5)
+
+#登录183684893627
+poco("com.thinkhome.v3:id/et_account").set_text("18368493627")
+poco("com.thinkhome.v3:id/et_password").set_text("123456")
+poco("com.thinkhome.v3:id/btn_login").click()
+poco(text="测试").click()
+sleep(5)
+poco(text="设置").click()
+poco(text="账号管理").click()
+
+
+# 拍照
+poco("com.thinkhome.v3:id/image_layout").click()
+poco(text="拍照").click()
+poco("com.huawei.camera:id/shutter_button").click()#考虑用图像识别
+poco("com.huawei.camera:id/btn_done").click()#考虑用图像识别
+poco("com.android.gallery3d:id/head_select_right").click()
+assert_exists(Template(r"tpl1533883870891.png", record_pos=(0.001, 0.528), resolution=(1080, 1920)), "拍照修改头像成功")
+
+# 选择本地图片
+poco("com.thinkhome.v3:id/image_layout").click()
+poco(text="选择本地图片").click()
+poco(text="最近").click()
+poco("com.android.documentsui:id/icon_mime").click()
+poco("com.android.gallery3d:id/head_select_right").click()
+assert_exists(Template(r"tpl1533883908278.png", record_pos=(0.002, 0.532), resolution=(1080, 1920)), "本地图片修改头像成功")
+
+# 取消修改
+poco("com.thinkhome.v3:id/image_layout").click()
+poco(text="取消").click()
+
+# 修改姓名：为空
+poco("com.thinkhome.v3:id/name").click()
+poco("com.thinkhome.v3:id/name").set_text("")
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+assert_exists(Template(r"tpl1533883588719.png", record_pos=(-0.001, 0.528), resolution=(1080, 1920)),"姓名为空验证通过")
+
+# 修改姓名：包含<字符
+poco("com.thinkhome.v3:id/name").set_text("<")
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+message = poco("android:id/message").get_text()
+assert_equal(message,"可能包含></等特殊字符，请修改重试。","姓名包含<验证通过")
+poco("android:id/button3").click()
+
+# 修改姓名：包含>字符
+poco("com.thinkhome.v3:id/name").set_text(">")
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+message = poco("android:id/message").get_text()
+assert_equal(message,"可能包含></等特殊字符，请修改重试。","姓名包含<验证通过")
+poco("android:id/button3").click()
+
+# 修改姓名：包含&字符
+poco("com.thinkhome.v3:id/name").set_text("&")
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+message = poco("android:id/message").get_text()
+assert_equal(message,"可能包含></等特殊字符，请修改重试。","姓名包含<验证通过")
+poco("android:id/button3").click()
+
+# 修改姓名：包含\字符
+poco("com.thinkhome.v3:id/name").set_text("\\")
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+message = poco("android:id/message").get_text()
+assert_equal(message,"可能包含></等特殊字符，请修改重试。","姓名包含<验证通过")
+poco("android:id/button3").click()
+
+# 正确修改姓名
+testName = "张三"
+poco("com.thinkhome.v3:id/name").set_text(testName)
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+realName = poco("com.thinkhome.v3:id/name").child("android.widget.RelativeLayout").child("android.widget.RelativeLayout").child("com.thinkhome.v3:id/value").get_text()
+assert_equal(testName,realName,"修改姓名成功")
+
+# 修改邮箱：为空
+poco("com.thinkhome.v3:id/email").click()
+poco("com.thinkhome.v3:id/name").set_text("")
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+assert_exists(Template(r"tpl1533883588719.png", record_pos=(-0.001, 0.528), resolution=(1080, 1920)),"邮箱为空验证通过")
+
+# 修改邮箱：格式不正确
+poco("com.thinkhome.v3:id/name").set_text("111111")
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+assert_exists(Template(r"tpl1533887453233.png", record_pos=(-0.005, 0.528), resolution=(1080, 1920)), "邮箱格式不正确验证通过")
+
+# 正确修改邮箱
+testEmail = "1234567@qq.com"
+poco("com.thinkhome.v3:id/name").set_text(testEmail)
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+realEmail = poco("com.thinkhome.v3:id/email").child("android.widget.RelativeLayout").child("android.widget.RelativeLayout").child("com.thinkhome.v3:id/value").get_text()
+assert_equal(testEmail,realEmail,"修改邮箱成功")
+
+# 修改密码：旧密码为空
+poco("com.thinkhome.v3:id/change_password").click()
+poco("com.thinkhome.v3:id/edit_pwd_new").set_text("123456")
+poco("com.thinkhome.v3:id/edit_pwd_new_confirm").set_text("123456")
+poco("com.thinkhome.v3:id/btn_modify").click()
+message = poco("android:id/message").get_text()
+assert_equal(message,"旧密码不能为空","旧密码为空验证通过")
+poco("android:id/button3").click()
+
+# 修改密码：新密码为空
+poco("com.thinkhome.v3:id/edit_pwd_old").set_text("123456")
+poco("com.thinkhome.v3:id/edit_pwd_new").set_text("")
+poco("com.thinkhome.v3:id/edit_pwd_new_confirm").set_text("123456")
+poco("com.thinkhome.v3:id/btn_modify").click()
+message = poco("android:id/message").get_text()
+assert_equal(message,"新密码不能为空","新密码为空验证通过")
+poco("android:id/button3").click()
+
+# 修改密码：确认密码为空
+poco("com.thinkhome.v3:id/edit_pwd_old").set_text("123456")
+poco("com.thinkhome.v3:id/edit_pwd_new").set_text("123456")
+poco("com.thinkhome.v3:id/edit_pwd_new_confirm").set_text("")
+poco("com.thinkhome.v3:id/btn_modify").click()
+message = poco("android:id/message").get_text()
+assert_equal(message,"确认密码不能为空","确认密码为空验证通过")
+poco("android:id/button3").click()
+
+# 修改密码：旧密码错误
+poco("com.thinkhome.v3:id/edit_pwd_old").set_text("999999")
+poco("com.thinkhome.v3:id/edit_pwd_new_confirm").set_text("123456")
+poco("com.thinkhome.v3:id/btn_modify").click()
+message = poco("android:id/message").get_text()
+assert_equal(message,"旧密码输入错误","旧密码错误验证通过")
+poco("android:id/button3").click()
+
+# 修改密码：新密码长度小于6位
+poco("com.thinkhome.v3:id/edit_pwd_old").set_text("123456")
+poco("com.thinkhome.v3:id/edit_pwd_new").set_text("12345")
+poco("com.thinkhome.v3:id/edit_pwd_new_confirm").set_text("12345")
+poco("com.thinkhome.v3:id/btn_modify").click()
+message = poco("android:id/message").get_text()
+assert_equal(message,"密码长度不能少于6位","新密码长度小于6位验证通过")
+poco("android:id/button3").click()
+
+# 修改密码：新密码与确认密码不一致
+poco("com.thinkhome.v3:id/edit_pwd_new").set_text("1234567")
+poco("com.thinkhome.v3:id/edit_pwd_new_confirm").set_text("12345678")
+poco("com.thinkhome.v3:id/btn_modify").click()
+message = poco("android:id/message").get_text()
+assert_equal(message,"两次新密码输入不一致","新密码与确认密码不一致验证通过")
+poco("android:id/button3").click()
+
+# 正确修改密码
+poco("com.thinkhome.v3:id/edit_pwd_new").set_text("123456")
+poco("com.thinkhome.v3:id/edit_pwd_new_confirm").set_text("123456")
+poco("com.thinkhome.v3:id/btn_modify").click()
+assert_exists(Template(r"tpl1533890116594.png", record_pos=(-0.003, 0.526), resolution=(1080, 1920)), "修改密码成功")
+
+# # 意见反馈：为空
+# poco("com.thinkhome.v3:id/feedback").click()
+# poco("com.thinkhome.v3:id/feedback_send").click()
+# message = poco("android:id/message").get_text()
+# assert_equal(message,"反馈内容不能为空","意见反馈为空验证通过")
+# poco("android:id/button3").click()
+# 
+# # 意见反馈成功
+# poco("com.thinkhome.v3:id/feedback").set_text("test")
+# poco("com.thinkhome.v3:id/feedback_send").click()
+# assert_exists(Template(r"tpl1533890705033.png", record_pos=(-0.007, 0.527), resolution=(1080, 1920)), "意见反馈成功")
+
+# 账号切换：添加账号、切换账号
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+poco("com.thinkhome.v3:id/add_text").click()
+poco("com.thinkhome.v3:id/et_account").set_text("18606683597")
+poco("com.thinkhome.v3:id/et_password").set_text("123456")
+poco("com.thinkhome.v3:id/btn_login").click()
+poco(text="我的家").click()
+sleep(2)
+poco(text="设置").click()
+poco(text="账号管理").click()
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+poco(text="18606683597").click()
+poco(text="18368493627").click()
+poco("com.thinkhome.v3:id/toolbar_btn_back").click()
+poco("com.thinkhome.v3:id/toolbar_btn_back").click()
+realAccount = poco("com.thinkhome.v3:id/account").child("android.widget.RelativeLayout").child("android.widget.RelativeLayout").child("com.thinkhome.v3:id/value").get_text()
+assert_equal("18606683597",realAccount,"切换账号返回验证通过")
+poco("com.thinkhome.v3:id/toolbar_right_text").click()
+poco(text="18368493627").click()
+poco(text="测试").click()
+sleep(5)
+poco(text="设置").click()
+poco(text="账号管理").click()
+realAccount = poco("com.thinkhome.v3:id/account").child("android.widget.RelativeLayout").child("android.widget.RelativeLayout").child("com.thinkhome.v3:id/value").get_text()
+assert_equal("18368493627",realAccount,"切换账号成功")
